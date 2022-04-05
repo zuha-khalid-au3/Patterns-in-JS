342a917085259a4f5e1f1d00d683193d498d2f76
unknown <zuhakhalid@gmail.com>
2022-04-05 15:51:01 +0530

    Proxy pattern set

--- Proxy/index.js
+++ Proxy/index.js
@@ -3,7 +3,17 @@ const person = {
     age: 100,
     city: "Allahabad"            
 };
-
-const personProxy = new Proxy(person,{});
-
+const handler = {
+    get:(target,prop,receiver)=>{
+        // console.log(`Getting prop: ${prop}`);
+        return target[prop];
+    },
+    set: (obj, prop, value) => { // value is the new value that is  assigned to the property
+        console.log(`Changed ${prop} from ${obj[prop]} to ${value}`);
+        obj[prop] = value;
+        return true;
+      }
+}
+const personProxy = new Proxy(person,handler);
+personProxy.name = "Zuha1111111111";
 console.log(personProxy.name);
